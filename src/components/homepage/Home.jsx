480d7cfdd997857ab1a5772e36433697deda7c0d
NishchayBhutani <bhutaninishchay9@gmail,com>
2022-09-28 01:39:11 +0530

    init commit

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import Header from "../header/Header";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/Header";
+import Navbar from "../navbar/Navbar";
+
+const List = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
+};
+
+export default List;
